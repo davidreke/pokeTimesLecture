84dd6398bb52b77449e8ac3664acb7bf3f688fb2
David Reke <david.e.reke@gmail.com>
2020-08-05 09:58:54 -0400

    Updates

---
+++ precache-manifest.7cfada5d2ea142e7725b06cd7f055a17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9fc06c3a5fd6ccd3f314733061957b1",
+    "url": "/pokeTimesLecture/index.html"
+  },
+  {
+    "revision": "9c621e6aa84dba12fbaa",
+    "url": "/pokeTimesLecture/static/css/main.a70f5d9a.chunk.css"
+  },
+  {
+    "revision": "e465e7e11667b7383ffc",
+    "url": "/pokeTimesLecture/static/js/2.a645739e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pokeTimesLecture/static/js/2.a645739e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c621e6aa84dba12fbaa",
+    "url": "/pokeTimesLecture/static/js/main.7224c9ff.chunk.js"
+  },
+  {
+    "revision": "753b435ee90aec3844b9",
+    "url": "/pokeTimesLecture/static/js/runtime-main.f61216cd.js"
+  },
+  {
+    "revision": "f10bf0780aecd8ff951259976c1784dd",
+    "url": "/pokeTimesLecture/static/media/pokeball.f10bf078.png"
+  }
+]);
